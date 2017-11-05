0c25b0c41499926db06a720cac88057eea8433dd
yasser mimouni <yasmimouni2@gmail.com>
2017-11-05 14:35:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ playlist = new Object();
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  return Object.assign({}, playlist, {artistName: songTitle});
+  return Object.assign({}, playlist, {[artistName]: songTitle});
 }
