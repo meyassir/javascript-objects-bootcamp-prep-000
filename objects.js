61393c252283e96c9d49c30a3b9f53aff7a118a0
yasser mimouni <yasmimouni2@gmail.com>
2017-11-05 14:40:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle)
   return Object.assign({}, playlist, {[artistName]: songTitle});
 }
 function removeFromPlaylist(playlist, artistName)
-{
-  return delete playlist[artistName];
+{ 
+  delete playlist[artistName];
+  return playlist;
 }
